aead4a842e823d5537bf21e592324ea6b88aaf1a
mpmoran2 <kamokumoran@gmail.com>
2021-06-02 15:51:59 -0700

    os.system opens created tts mp3 correctly

--- demo.py
+++ demo.py
@@ -1,5 +1,10 @@
 from gtts import gTTS
+import os
 
 text = "Oooo sheepy"
-gTTS(text=text,lang='en',slow=False)
+output = gTTS(text=text,lang='en',slow=False)
+output.save('output.mp3')
+
+os.system('output.mp3')
+# need to run in actual commandline not terminal
 
